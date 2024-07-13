4f67e75f7bf7e1a8f9a90eb85e8bb4fe876915d7
mohibrahman903@gmail.com <mohibrahman973@gmail.com>
2024-07-12 21:33:09 -0700

    done

---
+++ js-assignment-10/javascrit.js
@@ -0,0 +1,46 @@
+// 6 Find the Smallest Number in an Array
+// let array = [3, 5, 1, 8, 2];
+// let smallest = array[0];
+
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] < smallest) {
+//         smallest = array[i];
+//     }
+// }
+// console.log(smallest);
+
+// // 7 Convert Celcius to fahrenheit
+// var temp = +prompt('enter the temp in Fahrenheit');
+// var fahrenheit;
+// fahrenheit = (temp * 9/5)+32
+// console.log(fahrenheit);
+
+// // 8 Convert Fahrenheit to Celcius
+// var tempfahrenheit = +prompt('Enter the temp in celsuis');
+// var celsuis;
+// celsuis = (tempfahrenheit - 32) * 5/9;
+// console.log(celsuis);
+
+// 9 Reverse an Array
+// let array = [1, 2, 3, 4, 5];
+// let reversedArray = [];
+
+// for (let i = array.length - 1; i >= 0; i--) {
+//     reversedArray.push(array[i]);
+// }
+
+// console.log(reversedArray); 
+
+// 10 Calculate Average of an Array
+// function calculateAverage(arr) {
+//     if (arr.length === 0) return 0; // Handle empty array case
+//     let sum = arr.reduce((acc, val) => acc + val, 0);
+//     let average = sum / arr.length;
+//     return average;
+// }
+
+// // Example usage:
+// let numbers = [1, 2, 3, 4, 5];
+// let average = calculateAverage(numbers);
+// console.log(average); 
+
